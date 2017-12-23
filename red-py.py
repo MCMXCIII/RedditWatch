b9b4d1cecdceb7fe7a9348d48f958f7b0689204b
Chris Crawford <crawfordwcii@gmail.com>
2017-12-23 09:23:55 +0000

    4th commit: Full functiong script ready for crons!

--- red-py.py
+++ red-py.py
@@ -1,24 +1,16 @@
-import praw 
+import praw
 
-reddit = praw.Reddit(client_id='your-client-ID',
-                     client_secret='your-secret',
+reddit = praw.Reddit(client_id='svuvBEZgEgZhOA',
+                     client_secret='nn7D2e6ITDlkKE1PZy6W6VUUKaQ',
                      user_agent='something like firefox',
-		     username='Username for the account ',
-		     password='1234512345') 
+                     username='Watcher_Bot',
+                     password='1234512345')
 
+def alert():
+     message = "Submission:\n\n"
+     for submission in reddit.subreddit('BaltimoreAndDCr4r').new(limit=100):
+     	if "F4M" in submission.title:
+    		message = message + "[" + submission.title + "](" + submission.url + ")\n\n"
+     return message
 
-
-for submission in reddit.subreddit('aww').new(limit=150):
-     if "F4M" in submission.title:
-		print("cute stuff!")
-		print('_________________')
-		print(submission.title)
-		print(submission.url)
-
-
-#Checking for another topic!
-#for submission in reddit.subreddit('foxes').new(limit=250):
-#    print(submission.title)
-#    print(submission.url)
-
-
+reddit.redditor('Not_A_MadScientist').message('Alert!', alert())
